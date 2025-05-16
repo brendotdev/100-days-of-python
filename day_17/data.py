1f5be660f4fa8f0850a822b469342be2cf34619b
brendotdev <bnn.cdl@gmail.com>
2025-05-16 01:15:44 -0700

    chore: add sample question data for quiz game

---
+++ day_17/data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"text": "The sky is blue.", "answer": "True"},
+    {"text": "2 + 2 = 5.", "answer": "False"},
+    {"text": "Python is a type of snake.", "answer": "True"},
+    {"text": "There are 365 days in a year.", "answer": "True"},
+    {"text": "The moon is made of cheese.", "answer": "False"},
+]
