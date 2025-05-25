c2449d803f46b7200a16cdeb4aa395fc3ab4f4b9
brendotdev <bnn.cdl@gmail.com>
2025-05-24 22:43:24 -0700

    chore: provide sample question dataset for quiz application

---
+++ day_28/data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"text": "The sky is blue.", "answer": "True"},
+    {"text": "2 + 2 equals 5.", "answer": "False"},
+    {"text": "Python is a type of snake.", "answer": "True"},
+    {"text": "There are 26 letters in the English alphabet.", "answer": "True"},
+    {"text": "The Earth is flat.", "answer": "False"},
+]
